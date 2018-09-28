2deadb85b03ede2c4025cb0daafddf9cb9edbf39
17S 守屋　郁海 <171Y065@iscdm.local>
2018-09-28 13:52:19 +0900

    change showData

--- app/src/main/java/com/example/calendartest/showData.java
+++ app/src/main/java/com/example/calendartest/showData.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,6 +20,8 @@ public class showData extends AppCompatActivity {
     private TextView textView;
     private Database helper;
     int allMoney = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +36,7 @@ public class showData extends AppCompatActivity {
 
         final ArrayList data = new ArrayList();
         final SQLiteDatabase db = helper.getReadableDatabase();
-        final Cursor cursor = db.query(
+        Cursor cursor = db.query(
                 "Money",
                 new String[]{"_id","date","money"},
                 null,
@@ -47,7 +48,7 @@ public class showData extends AppCompatActivity {
         cursor.moveToFirst();
 
         StringBuilder sbuilder = new StringBuilder();
-        int id =0;
+        final ArrayList<Integer> arrayList = new ArrayList<>();
 
         for(int i = 0; i < cursor.getCount();i++){
             /*
@@ -56,19 +57,55 @@ public class showData extends AppCompatActivity {
             sbuilder.append(cursor.getInt(1));
             sbuilder.append("円\n\n");
             */
-            id = cursor.getInt(0);
+            arrayList.add(cursor.getInt(0));
             allMoney += cursor.getInt(2);
-            Log.e("", String.valueOf(id),null);
-            data.add(cursor.getString(1) + ":  "+ cursor.getInt(2) + "円");
+            data.add(arrayList.get(i) + cursor.getString(1) + ":  "+ cursor.getInt(2) + "円");
             cursor.moveToNext();
         }
         data.add("累計:　　　   " + allMoney + "円");
+
+/*
+        Calendar cl = Calendar.getInstance();
+
+        int clMonth = cl.get(Calendar.MONTH)+1;
+        int dateclMonth = cl.get(Calendar.MONTH)+2;
+
+        int clDay = cl.get(Calendar.DAY_OF_MONTH);
+
+        String clmonth = String.valueOf(clMonth);
+        String clday = String.valueOf(clDay);
+        String dateclmonth = String.valueOf(dateclMonth);
+
+        if(dateclMonth <10){
+            dateclmonth = "0" + dateclmonth;
+        }
+        if(clMonth < 10){
+            clmonth = "0" + clmonth;
+        }
+        if(clDay < 10){
+            clday = "0" + clday;
+        }
+
+        String cal = cl.get(Calendar.YEAR)  + "-" + clmonth;
+        String endcal = cl.get(Calendar.YEAR) + "-" + dateclmonth;
+        Log.d("",cal + "\n" + endcal ,null);
+        Cursor cursor1 = db.rawQuery("SELECT * FROM Money WHERE STRFTIME('%Y-%m'," + endcal + ") >= " + "STRFTIME('%Y-%m',"+ cal + ")",null);
+        cursor1.moveToFirst();
+        int monthMoney =0;
+        for(int i =0; i< cursor1.getCount(); i++){
+            monthMoney += cursor1.getInt(2);
+            Log.d("DB_RawQuery", String.valueOf(cursor1.getInt(2)),null);
+            cursor1.moveToNext();
+        }
+        data.add("月:　　　　   " + monthMoney + "円");
+
+        //where strftime('YYYY-mm'," + endcal + ") > " + "strftime('YYYY-mm',"+ cal +")
+*/
 
         final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,data);
         ListView listView =(ListView)findViewById(R.id.showList);
         listView.setAdapter(adapter);
 
-        final int finalId = id;
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long l) {
@@ -77,10 +114,10 @@ public class showData extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         data.remove(position);
-                        db.delete("money", "_id=" + finalId, null);
+                        db.delete("money", "_id=" + arrayList.get(position), null);
+                        arrayList.remove(position);
                         adapter.notifyDataSetChanged();
                         Toast.makeText(showData.this,"削除しました",Toast.LENGTH_LONG);
-
                     }
                 }).setNegativeButton("キャンセル",null).setCancelable(true);
                 builder.show();
